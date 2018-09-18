d648f166f1e7688552c7cdcef2fff3887abdefcc
Martin Ottenwaelter <martin.ottenwaelter@gmail.com>
2018-09-18 17:32:51 +0200

    update public key

--- _etc/install.sh
+++ _etc/install.sh
@@ -5,6 +5,6 @@ apt-get -y install convmv ca-certificates libcap2-bin
 
 adduser --disabled-password --gecos "" martin
 su - martin -c 'mkdir /home/martin/.ssh'
-su - martin -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAw9R1/dJA3wrhZ5fCTWBg5gVZhfleWQJ6bDMfplTZ7TjCYuq0/KkjYGGAxB4IplR0NMeVAjfrs2RWMuUSwDmI3Fr+y1xVrHWdwpESciOvx7k0YnVhETIxbLmnVCSkcTzyYCjdmQvxNwElkr55TEt+1zVpWMNTx9d5bNjcgXoaZyqAM4PTF2O9KCOiUOVsiklygCM6GY4dVAC/Z3+Xhsp4/q/wojGlNEzjtKQAD6OXD3ogmQl9TPAURo7QdOtGhIYo6sp7eq4XtsdidSHCNPaXsS4d6MM9+LTXtVlxzoBwRFiw4k/625BCLj4RnlDbu+vBvY6ZtCnj5I/rRm7MQsIC+w== martin" >> /home/martin/.ssh/authorized_keys'
+su - martin -c 'echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMfCM9I5xPrH5bl1TyjbCaqNPALl3cTU1e4NeJNqHTK3 martin" >> /home/martin/.ssh/authorized_keys'
 
 su - martin -c 'mkdir /home/martin/ahouhpuc'
