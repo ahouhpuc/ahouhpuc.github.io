5015a5fce56dc08d394e0f08e3ad97d47086e27b
Martin Salles <salles.martin@gmail.com>
2020-03-30 19:22:04 +0200

    Add spambot filter

--- _etc/go.mod
+++ _etc/go.mod
@@ -1,3 +1,5 @@
 module server
 
+go 1.14
+
 require golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b
--- _etc/mail.go
+++ _etc/mail.go
@@ -13,6 +13,8 @@ var email = os.Getenv("AOP_EMAIL_ADDRESS")
 var login = os.Getenv("AOP_EMAIL_LOGIN")
 var password = os.Getenv("AOP_EMAIL_PASSWORD")
 
+var expectedScore = "7";
+
 func sendMail(visitorEmail string, body string) error {
 	auth := smtp.PlainAuth(
 		"",
@@ -42,9 +44,13 @@ func mailHandler(w http.ResponseWriter, r *http.Request) {
 	visitorEmail := r.PostFormValue("email")
 	body := r.PostFormValue("body")
 	redirect := r.PostFormValue("redirect")
+	score := r.PostFormValue("score") // Our score input is filtering spambots (a bit)
 
 	if len(visitorEmail) > 0 && len(body) > 0 && len(redirect) > 0 {
-		err := sendMail(visitorEmail, body)
+		var err = error(nil)
+		if (score == expectedScore) {
+			err = sendMail(visitorEmail, body)
+		}
 		if err == nil {
 			http.Redirect(w, r, redirect, http.StatusFound)
 		} else {
